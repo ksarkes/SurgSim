ec3319dc192582011c8e1f3bcd4478a44683f0c7
ksarkes <ksarkes@hotmail.com>
2016-04-09 17:37:30 +0300

    Init

---
+++ Assets/customColision.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class customColision : MonoBehaviour {
+
+    Vector3 averageNormal;
+    GameObject otherObj;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionStay(Collision col)
+    {
+        Debug.Log("Collision motherfucker");
+        if (col.collider.gameObject.tag != "static")
+        {
+            foreach (ContactPoint point in col.contacts)
+            {
+                otherObj = point.otherCollider.gameObject;
+                averageNormal += point.normal;
+            }
+            Debug.Log("Collision detecting, adding force");
+            otherObj.GetComponent<Rigidbody>().AddRelativeForce(averageNormal);
+        }
+    }
+}
---
+++ Assets/scripts/Drag_n_drop.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drag_n_drop : MonoBehaviour {
+
+    GameObject target;
+    bool isMouseDrag;
+    private Vector3 screenPosition;
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            target = ReturnClickedObject(out hitInfo);
+            if (target != null)
+            {
+                isMouseDrag = true;
+                Debug.Log("target position :" + target.transform.position);
+                //Convert world position to screen position.
+                screenPosition = Camera.main.WorldToScreenPoint(target.transform.position);
+                offset = target.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPosition.z));
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            isMouseDrag = false;
+        }
+        if (isMouseDrag)
+        {
+            //track mouse position.
+            Vector3 currentScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPosition.z);
+            //convert screen position to world position with offset changes.
+            Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenSpace) + offset;
+            //It will update target gameobject's current postion.
+            target.transform.position = currentPosition;
+        }
+    }
+
+
+
+    GameObject ReturnClickedObject(out RaycastHit hit)
+    {
+        GameObject target = null;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray.origin, ray.direction * 10, out hit))
+        {
+            target = hit.collider.gameObject;
+        }
+        if (target.tag == "static")
+        {
+            return null;
+        }
+        return target;
+    }
+
+}
