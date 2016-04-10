3bc9f8a5327e734339eff5c4b6410a5cd3cd2ae3
ksarkes <ksarkes@hotmail.com>
2016-04-11 01:18:12 +0300

    hit detector

---
+++ Assets/triggerBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triggerBehaviour : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "vessel")
+            Debug.Log("Pretty hit");
+    }
+}
